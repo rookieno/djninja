e41d28f329e0d1e76566353d9d108f1deef83664
rookieno <rhkdals3257@naver.com>
2022-02-10 13:19:42 +0900

    update

--- ninja_prac/wsgi.py
+++ ninja_prac/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ninja_prac.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ninja_prac.settings")
 
 application = get_wsgi_application()
