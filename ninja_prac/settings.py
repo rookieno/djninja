b8e5a44aeec176d6b834e007b7fc7930fe0af359
rookieno <rhkdals3257@naver.com>
2022-02-10 13:42:25 +0900

    test update

--- ninja_prac/settings.py
+++ ninja_prac/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from typing import List
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -25,7 +27,7 @@ SECRET_KEY = "django-insecure-2j#dnm_)e3nklh)36ab#g*6h4anye018p35yw2q%kk%1j^d@tz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: List[str] = []
 
 
 # Application definition
