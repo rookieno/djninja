2b3522b0e940444e778c1ed1042de9fc6e16ee1d
rookieno <rhkdals3257@naver.com>
2022-02-21 15:58:39 +0900

    use prefetch for single view

--- tabom/services/article_service.py
+++ tabom/services/article_service.py
@@ -1,17 +1,18 @@
-from django.core.paginator import Page, Paginator
 from django.db.models import Prefetch, QuerySet
 
 from tabom.models import Article, Like
 
 
-def get_an_article(article_id: int) -> Article:
-    return Article.objects.filter(id=article_id).get()
+def get_an_article(user_id: int, article_id: int) -> Article:
+    return Article.objects.prefetch_related(
+        Prefetch("like_set", queryset=Like.objects.filter(user_id=user_id), to_attr="my_likes")
+    ).get(id=article_id)
 
 
 def get_article_list(user_id: int, offset: int, limit: int) -> QuerySet[Article]:
     return (
         Article.objects.order_by("-id")
-        .prefetch_related("like_set")[offset : offset + limit]
+        .prefetch_related("like_set")
         .prefetch_related(Prefetch("like_set", queryset=Like.objects.filter(user_id=user_id), to_attr="my_likes"))[
             offset : offset + limit
         ]
--- tabom/tests/test_article_service.py
+++ tabom/tests/test_article_service.py
@@ -1,9 +1,8 @@
 from django.test import TestCase
 
+from tabom.models import Like, User
 from tabom.models.article import Article
-from tabom.models.user import User
 from tabom.services.article_service import get_an_article, get_article_list
-# get_article_page)
 from tabom.services.like_service import do_like
 
 
@@ -14,7 +13,7 @@ class TestArticleService(TestCase):
         article = Article.objects.create(title=title)
 
         # When
-        result_article = get_an_article(article.id)
+        result_article = get_an_article(0, article.id)
 
         # Then
         self.assertEqual(article.id, result_article.id)
@@ -26,7 +25,7 @@ class TestArticleService(TestCase):
 
         # Expect
         with self.assertRaises(Article.DoesNotExist):
-            get_an_article(invalid_article_id)
+            get_an_article(0, invalid_article_id)
 
     def test_get_article_list_should_prefetch_like(self) -> None:
         # Given
@@ -34,20 +33,20 @@ class TestArticleService(TestCase):
         articles = [Article.objects.create(title=f"{i}") for i in range(1, 21)]
         do_like(user.id, articles[-1].id)
 
-        # Wgen
+        # When
         with self.assertNumQueries(3):
             result_articles = get_article_list(user.id, 0, 10)
-            result_count = [a.like_set.count() for a in result_articles]
+            result_counts = [a.like_set.count() for a in result_articles]
 
             # Then
             self.assertEqual(len(result_articles), 10)
-            self.assertEqual(1, result_count[0])
+            self.assertEqual(1, result_counts[0])
             self.assertEqual(
                 [a.id for a in reversed(articles[10:21])],
                 [a.id for a in result_articles],
             )
 
-    def test_get_article_list_should_contain_my_like_when_like_exists(self) -> None:
+    def test_get_article_list_should_contain_my_likes_when_like_exists(self) -> None:
         # Given
         user = User.objects.create(name="test_user")
         article1 = Article.objects.create(title="artice1")
@@ -61,19 +60,17 @@ class TestArticleService(TestCase):
         self.assertEqual(like.id, articles[1].my_likes[0].id)
         self.assertEqual(0, len(articles[0].my_likes))
 
-    # def test_get_article_page_should_prefetch_like(self) -> None:
-    #     # Given
-    #     user = User.objects.create(name="test_user")
-    #     articles = [Article.objects.create(title=f"{i}") for i in range(1, 21)]
-    #     do_like(user.id, articles[-1].id)
-    #
-    #     # Wgen
-    #     result_articles = get_article_page(1, 10)
-    #
-    #     # Then
-    #     self.assertEqual(len(result_articles), 10)
-    #     self.assertEqual(1, result_articles[0].like_set.count())
-    #     self.assertEqual(
-    #         [a.id for a in reversed(articles[10:21])],
-    #         [a.id for a in result_articles],
-    #     )
+    def test_get_article_list_should_not_contain_my_likes_when_user_id_is_zero(self) -> None:
+        # Given
+        user = User.objects.create(name="test_user")
+        article1 = Article.objects.create(title="artice1")
+        Like.objects.create(user_id=user.id, article_id=article1.id)
+        Article.objects.create(title="article2")
+        invalid_user_id = 0
+
+        # When
+        articles = get_article_list(invalid_user_id, 0, 10)
+
+        # Then
+        self.assertEqual(0, len(articles[1].my_likes))
+        self.assertEqual(0, len(articles[0].my_likes))
